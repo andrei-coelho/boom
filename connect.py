360b2372f7e8399e4c800088264d994155757ca2
unknown <andreifcoelho@gmail.com>
2022-03-16 11:12:09 -0300

    pequenna modificação

--- connect.py
+++ connect.py
@@ -11,7 +11,7 @@ def start():
     scren = auto.size()
     xMiddleScren = int(scren.width / 2)
     yMiddleScren = int(scren.height / 2)
-    posConnect = yMiddleScren + 300
+    posConnect = yMiddleScren + 260
 
     auto.moveTo(xMiddleScren, posConnect, duration=0.5)
     auto.click()
